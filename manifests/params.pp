dcf93cd7045e70032858ae1a0e3fed82f8925f29
Zach Leslie <zach@puppetlabs.com>
2012-12-26 13:05:41 -0800

    improve the handling of certificate paths in the ssl module

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,17 @@
 class ssl::params {
 
-  case $operatingsystem {
-    'centos', 'fedora', 'redhat': {
-       $ssl_path = '/etc/pki'
+  case $osfamily {
+    'redhat': {
+      $ssl_path      = '/etc/pki'
+      $ssl_cert_file = hiera('ssl_cert_file', "${ssl_path}/tls/certs/localhost.crt")
+      $ssl_key_file  = hiera('ssl_key_file',  "${ssl_path}/tls/private/localhost.key")
     }
-    'ubuntu', 'debian': {
-       $ssl_path = '/etc/ssl'
+    'debian': {
+      $ssl_path      = '/etc/ssl'
+      $ssl_cert_file = hiera('ssl_cert_file', "${ssl_path}/certs/ssl-cert-snakeoil.pem")
+      $ssl_key_file  = hiera('ssl_key_file',  "${ssl_path}/private/ssl-cert-snakeoil.key")
     }
     default: { $ssl_path = '/etc/ssl' }
   }
+
 }
