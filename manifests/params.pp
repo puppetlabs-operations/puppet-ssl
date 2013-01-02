1142e1119d8a27418b55433b364e86bde5a69743
Zach Leslie <zach@puppetlabs.com>
2013-01-02 10:57:10 -0800

    move variables for ssl::params into heira

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,7 @@
-class ssl::params {
-
-  case $osfamily {
-    'redhat': {
-      $ssl_path      = '/etc/pki'
-      $ssl_cert_file = hiera('ssl_cert_file', "${ssl_path}/tls/certs/localhost.crt")
-      $ssl_key_file  = hiera('ssl_key_file',  "${ssl_path}/tls/private/localhost.key")
-    }
-    'debian': {
-      $ssl_path      = '/etc/ssl'
-      $ssl_cert_file = hiera('ssl_cert_file', "${ssl_path}/certs/ssl-cert-snakeoil.pem")
-      $ssl_key_file  = hiera('ssl_key_file',  "${ssl_path}/private/ssl-cert-snakeoil.key")
-    }
-    default: { $ssl_path = '/etc/ssl' }
-  }
+class ssl::params(
+  $ssl_path,
+  $ssl_cert_file,
+  $ssl_key_file
+){
 
 }
