f75c1b640236fb43a001ebdaf9894ca9578f9838
James Abley <james.abley@gmail.com>
2014-01-19 12:52:58 +0000

    Add ssl_ciphers
    I would like to evolve my nginx SSL configuration
    over time.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,7 @@ class ssl::params {
   # this value.
   $ssl_path      = hiera('ssl::params::ssl_path')
   $ssl_cert_file = hiera('ssl::params::ssl_cert_file')
+  $ssl_ciphers   = hiera('ssl::params::ssl_ciphers')
   $ssl_key_file  = hiera('ssl::params::ssl_key_file')
 
 }
