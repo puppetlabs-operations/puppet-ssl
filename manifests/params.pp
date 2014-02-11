c8b87a58c6c7f46645de394816fe408dacf06a08
Zach Leslie <zach@puppetlabs.com>
2014-02-11 11:19:33 -0800

    Import code from private PL Ops repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,43 @@
+# Class: ssl::params
+#
+# Sets paramters for SSL used in the deployment of keypairs.
+#
 class ssl::params {
+  case $::osfamily {
+    'debian': {
+      $ssl_dir              = '/etc/ssl'
+      $ssl_certdir          = "${ssl_dir}/certs"
+      $ssl_keydir           = "${ssl_dir}/private"
+      $ssl_default_certpath = "${ssl_certdir}/ssl-cert-snakeoil.pem"
+      $ssl_default_keypath  = "${ssl_keydir}/ssl-cert-snakeoil.key"
+    }
+    'redhat': {
+      $ssl_dir              = '/etc/pki'
+      $ssl_certdir          = "${ssl_dir}/certs"
+      $ssl_keydir           = "${ssl_dir}/private"
+      $ssl_default_certpath = "${ssl_certdir}/localhost.crt"
+      $ssl_default_keypath  = "${ssl_keydir}/localhost.key"
+    }
+    'freebsd': {
+      $ssl_dir              = '/etc/ssl'
+      $ssl_certdir          = "${ssl_dir}/certs"
+      $ssl_keydir           = "${ssl_dir}/private"
+      $ssl_default_certpath = "${ssl_certdir}/localhost.crt"
+      $ssl_default_keypath  = "${ssl_keydir}/localhost.key"
+    }
+    default: {
+      notice("ssl::params does not support ${::osfamily}.")
+    }
+  }
 
-  # This has to be resolved before other variables because they may interpolate
-  # this value.
-  $ssl_path      = hiera('ssl::params::ssl_path')
-  $ssl_cert_file = hiera('ssl::params::ssl_cert_file')
-  $ssl_ciphers   = hiera('ssl::params::ssl_ciphers')
-  $ssl_key_file  = hiera('ssl::params::ssl_key_file')
-
+  # This class cannot use the standard data binding lookup because ssl_path
+  # is interpolated into the other values and thus must be loaded into scope
+  # first.
+  #
+  # We should prepare to deprecate these first few variables
+  $ssl_path       = $ssl_dir
+  $ssl_cert_file  = hiera('ssl::params::ssl_cert_file')
+  $ssl_chain_file = hiera('ssl::params::ssl_chain_file')
+  $ssl_key_file   = hiera('ssl::params::ssl_key_file')
+  $ssl_ciphers    = hiera('ssl::params::ssl_ciphers')
 }
